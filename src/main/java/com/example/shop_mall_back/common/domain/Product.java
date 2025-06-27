08cbad1f24c524432b3cac7df9c73932d4fc9baf
신인호 <strongbox1@naver.com>
2025-06-27 14:55:17 +0900

    domain 폴더 추가1

---
+++ src/main/java/com/example/shop_mall_back/common/domain/Product.java
@@ -0,0 +1,52 @@
+package com.example.shop_mall_back.common.domain;
+
+import com.example.shop_mall_back.user.product.domain.Brand;
+import com.example.shop_mall_back.user.product.domain.Category;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private int price;
+
+    @Column(nullable = false)
+    private int stock = 0;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    @Column(nullable = false)
+    private int viewCount = 0;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "sell_status")
+    private SellStatus sellStatus;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "delivery_id")
+    private DeliveryInfo deliveryInfo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category")
+    private Category category;
+
+    public enum SellStatus {
+        SELLING, SOLD_OUT, STOPPED
+    }
+}
