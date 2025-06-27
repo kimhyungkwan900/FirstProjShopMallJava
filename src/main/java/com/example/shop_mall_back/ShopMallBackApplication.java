82d62a5c93a8f220e924ed0b27b93b738de16b15
김형관 <bearson0115@gmail.com>
2025-06-27 11:10:24 +0900

    first_commit_2025_06_27

---
+++ src/main/java/com/example/shop_mall_back/ShopMallBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.shop_mall_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMallBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopMallBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shop_mall_back/ShopMallBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shop_mall_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMallBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
