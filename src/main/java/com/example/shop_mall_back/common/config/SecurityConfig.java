75e3bda317725c935861dc2cbf0fd78d9a0fe037
김형관 <bearson0115@gmail.com>
2025-06-30 17:47:03 +0900

    2025_06_30_1746

--- src/main/java/com/example/shop_mall_back/common/config/SecurityConfig.java
+++ src/main/java/com/example/shop_mall_back/common/config/SecurityConfig.java
@@ -1,11 +1,17 @@
 package com.example.shop_mall_back.common.config;
 
+import com.example.shop_mall_back.common.service.oauthService.KakaoOAuthService;
 import com.querydsl.core.annotations.Config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -15,5 +21,28 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean   //TODO: defaultSuccessUrl 메인페이지로 변경할 것
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())               //csrf 비활성화 TODO: 개발 종료후 삭제
+                .authorizeHttpRequests(auth -> auth    //URL 접근권한 설정 아래 requestMatchers 는 인증없이 접근 가능
+                        .requestMatchers("/oauth2/**","login/**","/css/**","/js/**", "/images/**").permitAll()
+                        .anyRequest().authenticated()                               //그 외 모든 요청 인증필요
+                )
+                .oauth2Login(oauth2 -> oauth2       // OAuth2 Login
+                        .defaultSuccessUrl("/main",true)    // 로그인 이후 Redirection 경로
+                        .userInfoEndpoint(userInfo ->          // 사용자정보 -> oauth2UserService 에서 처리
+                                userInfo.userService(oauth2UserService())
+                        )
+                );
+
+        return http.build();
+    }
+
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
+        return new KakaoOAuthService();
     }
 }
--- src/main/java/com/example/shop_mall_back/common/service/oauthService/KakaoOAuthService.java
+++ src/main/java/com/example/shop_mall_back/common/service/oauthService/KakaoOAuthService.java
@@ -1,4 +1,60 @@
 package com.example.shop_mall_back.common.service.oauthService;
 
-public class KakaoOAuthService {
+import com.example.shop_mall_back.common.domain.Member;
+import com.example.shop_mall_back.common.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class KakaoOAuthService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2User oAuth2User = new DefaultOAuth2UserService().loadUser(userRequest);
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+
+        String email = (String) kakaoAccount.get("email");
+        String phone = (String) kakaoAccount.get("phone_number");
+        String nickname = (String) profile.get("nickname");
+
+        Optional<Member> member = Optional.ofNullable(memberRepository.findByEmail(email));
+
+        Member newMember = member.orElseGet(() -> {
+           Member createdMember = Member.create(
+                   nickname,
+                   UUID.randomUUID().toString(),
+                   email,
+                   phone != null ? phone : "010-0000-0000",
+                   passwordEncoder
+           );
+            return memberRepository.save(createdMember);
+        });
+
+
+        return new DefaultOAuth2User(
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
+                oAuth2User.getAttributes(),
+                "id"
+        );
+    }
 }
