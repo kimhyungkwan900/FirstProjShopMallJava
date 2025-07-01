731ff5b7af73518374115b1512ada282c1cd4112
kim Gun ho <vlfh0002@gmail.com>
2025-07-01 14:47:12 +0900

    review 좋아요/싫어요 매핑 및 리뷰 목록에 좋아요 싫어요 수 카운트 추가

--- src/main/java/com/example/shop_mall_back/user/review/repository/ReviewReactionRepository.java
+++ src/main/java/com/example/shop_mall_back/user/review/repository/ReviewReactionRepository.java
@@ -2,6 +2,7 @@ package com.example.shop_mall_back.user.review.repository;
 
 import com.example.shop_mall_back.user.review.domain.Review;
 import com.example.shop_mall_back.user.review.domain.ReviewReaction;
+import com.example.shop_mall_back.user.review.domain.enums.Reaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,5 @@ import java.util.Optional;
 @Repository
 public interface ReviewReactionRepository extends JpaRepository<ReviewReaction,Long> {
     Optional<ReviewReaction> findByMemberIdAndReview(Long memberId, Review review);
+    int countByReview_IdAndReaction(Long reviewId, Reaction reaction);
 }
