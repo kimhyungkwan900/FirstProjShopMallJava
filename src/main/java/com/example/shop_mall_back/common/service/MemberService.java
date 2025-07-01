646f9ed55315a8bcf07fe3755ec3bb929e23c238
김형관 <bearson0115@gmail.com>
2025-07-02 05:26:00 +0900

    2025_07_02_0525

--- src/main/java/com/example/shop_mall_back/common/service/MemberService.java
+++ src/main/java/com/example/shop_mall_back/common/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.example.shop_mall_back.common.service;
 
+import com.example.shop_mall_back.common.constant.Role;
 import com.example.shop_mall_back.common.domain.Member;
+import com.example.shop_mall_back.common.dto.MemberDTO;
 import com.example.shop_mall_back.common.dto.MemberFormDTO;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -12,19 +14,40 @@ public interface MemberService {
 
     MemberFormDTO getMemberForm(String email);
 
+    MemberDTO getMemberDTOByEmail(String email);
+
+    Role getRoleByMember(Member member);
+
+    Member authenticate(String userId, String rawPassword);
+
     void memberFormUpdate(MemberFormDTO memberFormDTO,PasswordEncoder passwordEncoder);
 
     void deActivateMember(Long id);
 
     void activateMember(Long id);
 
-    default MemberFormDTO entityToDTO(Member member) {
+    default MemberFormDTO entityToDTOMemberForm(Member member) {
         return MemberFormDTO.builder()
                 .id(member.getId())
                 .user_id(member.getUserId())
                 .user_password(member.getUserPassword())
                 .email(member.getEmail())
                 .phone_number(member.getPhoneNumber())
+                .build();
+    }
+
+    // Member -> MemberDTO
+    default MemberDTO entityToDTOMember(Member member){
+        return MemberDTO.builder()
+                .id(member.getId())
+                .userId(member.getUserId())
+                .email(member.getEmail())
+                .emailVerified(member.isEmailVerified())
+                .phoneNumber(member.getPhoneNumber())
+                .phoneNumberVerified(member.isPhoneVerified())
+                .oauthProvider(member.getOauthProvider())
+                .isActive(member.isActive())
+                .createdAt(member.getCreatedAt())
                 .build();
     }
 
