68a0c85439fa22c262940fdca948f7735816f470
jungyouri <jyouri0306@naver.com>
2025-07-04 18:05:49 +0900

    25-07-04

--- src/main/java/com/example/shop_mall_back/admin/faq/dto/PageRequestDto.java
+++ src/main/java/com/example/shop_mall_back/admin/faq/dto/PageRequestDto.java
@@ -1,4 +1,21 @@
 package com.example.shop_mall_back.admin.faq.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PageRequestDto {
+
+    @Builder.Default
+    private int page = 1;
+
+    @Builder.Default
+    private int size = 10;
+
+
 }
