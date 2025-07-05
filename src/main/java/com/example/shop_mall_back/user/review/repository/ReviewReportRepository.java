a60a095a0f4ce3b3fbf58f37d95d1b7469f62c3d
kim Gun ho <vlfh0002@gmail.com>
2025-07-05 17:21:49 +0900

    관리자 신고리스트 리뷰 수정

--- src/main/java/com/example/shop_mall_back/user/review/repository/ReviewReportRepository.java
+++ src/main/java/com/example/shop_mall_back/user/review/repository/ReviewReportRepository.java
@@ -1,10 +1,16 @@
 package com.example.shop_mall_back.user.review.repository;
 
+import com.example.shop_mall_back.admin.review.dto.AdminReviewReportDTO;
 import com.example.shop_mall_back.user.review.domain.ReviewReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReviewReportRepository extends JpaRepository<ReviewReport, Long> {
     int countByReviewId(Long reviewId);
+
+    List<ReviewReport> findAllByReviewId(Long reviewId);
+
 }
