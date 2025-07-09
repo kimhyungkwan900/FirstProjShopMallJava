66c257bd84e554e865be5fa72561680a7b5a0fc4
shchae <snghynchae@naver.com>
2025-07-09 20:49:39 +0900

    2025-07-09 2049

--- src/main/java/com/example/shop_mall_back/admin/product/dto/ProductDto.java
+++ src/main/java/com/example/shop_mall_back/admin/product/dto/ProductDto.java
@@ -4,10 +4,12 @@ import com.example.shop_mall_back.common.domain.Product;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 @Builder
 public class ProductDto {
