d78d6fbae0f024a1b9e0689b702818c16c6d556e
김형관 <bearson0115@gmail.com>
2025-07-14 18:07:13 +0900

    2025_07_14_1807

---
+++ src/main/java/com/example/shop_mall_back/user/Cart/data/DeliveryInfoEx.sql
@@ -0,0 +1,8 @@
+INSERT INTO delivery_info (delivery_yn, delivery_com, delivery_price)
+VALUES ('Y', 'CJ', 3000);
+
+INSERT INTO delivery_info (delivery_yn, delivery_com, delivery_price)
+VALUES ('Y', 'LOTTE', 3500);
+
+INSERT INTO delivery_info (delivery_yn, delivery_com, delivery_price)
+VALUES ('Y', 'LOGEN', 3200);
